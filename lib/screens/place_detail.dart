51ec2aa007a8eba77de05ddb52bebfe2ec9b5289
oandeliz <59943720+oandeliz@users.noreply.github.com>
2024-03-29 20:54:16 -0400

    add MapScreen and also made the map avatar go to the map screen on touch

--- lib/screens/place_detail.dart
+++ lib/screens/place_detail.dart
@@ -1,4 +1,5 @@
 import 'package:favorite_places/models/place.dart';
+import 'package:favorite_places/screens/maps.dart';
 import 'package:flutter/material.dart';
 
 class PlaceDetailScreen extends StatelessWidget {
@@ -33,8 +34,21 @@ class PlaceDetailScreen extends StatelessWidget {
               right: 0,
               child: Column(
                 children: [
-                  CircleAvatar(
-                      radius: 70, backgroundImage: NetworkImage(locationImage)),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (ctx) => MapScreen(
+                            location: place.location,
+                            isSelecting: false,
+                          ),
+                        ),
+                      );
+                    },
+                    child: CircleAvatar(
+                        radius: 70,
+                        backgroundImage: NetworkImage(locationImage)),
+                  ),
                   Container(
                     alignment: Alignment.center,
                     padding: const EdgeInsets.symmetric(
